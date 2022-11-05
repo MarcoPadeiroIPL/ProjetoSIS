91e69691ff8f235fa386c438c7bc3c06fd795dc0
MarcoPadeiroIPL <2211868@my.ipleiria.pt>
2022-11-05 15:00:51 +0000

    Ajuste da sidebar para as funcionalidades pretendidas Mudanca no design do login Confirm pop-up no logout

--- backend/views/layouts/sidebar.php
+++ backend/views/layouts/sidebar.php
@@ -1,8 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="<?=$assetDir?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light">Airbender</span>
     </a>
 
     <!-- Sidebar -->
@@ -10,10 +9,10 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="<?=$assetDir?>/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="<?= $assetDir ?>/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
+                <a href="#" class="d-block"><?= Yii::$app->user->identity->username ?></a>
             </div>
         </div>
 
@@ -36,42 +35,52 @@
             echo \hail812\adminlte\widgets\Menu::widget([
                 'items' => [
                     [
-                        'label' => 'Starter Pages',
+                        'label' => 'Dashboard',
                         'icon' => 'tachometer-alt',
-                        'badge' => '<span class="right badge badge-info">2</span>',
-                        'items' => [
-                            ['label' => 'Active Page', 'url' => ['site/index'], 'iconStyle' => 'far'],
-                            ['label' => 'Inactive Page', 'iconStyle' => 'far'],
-                        ]
+                        'url' => ['site/index']
                     ],
-                    ['label' => 'Simple Link', 'icon' => 'th', 'badge' => '<span class="right badge badge-danger">New</span>'],
-                    ['label' => 'Yii2 PROVIDED', 'header' => true],
-                    ['label' => 'Login', 'url' => ['site/login'], 'icon' => 'sign-in-alt', 'visible' => Yii::$app->user->isGuest],
-                    ['label' => 'Gii',  'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
-                    ['label' => 'Debug', 'icon' => 'bug', 'url' => ['/debug'], 'target' => '_blank'],
-                    ['label' => 'MULTI LEVEL EXAMPLE', 'header' => true],
-                    ['label' => 'Level1'],
+                    ['label' => 'MANAGE', 'header' => true],
                     [
-                        'label' => 'Level1',
+                        'label' => 'Employees',
+                        'icon' => 'fa-solid fa-user-tie',
                         'items' => [
-                            ['label' => 'Level2', 'iconStyle' => 'far'],
-                            [
-                                'label' => 'Level2',
-                                'iconStyle' => 'far',
-                                'items' => [
-                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle'],
-                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle'],
-                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle']
-                                ]
-                            ],
-                            ['label' => 'Level2', 'iconStyle' => 'far']
+                            ['label' => 'Ticket Operators', 'iconStyle' => 'far'],
+                            ['label' => 'Supervisors', 'iconStyle' => 'far'],
+                            ['label' => 'Admins', 'iconStyle' => 'far']
                         ]
                     ],
-                    ['label' => 'Level1'],
-                    ['label' => 'LABELS', 'header' => true],
-                    ['label' => 'Important', 'iconStyle' => 'far', 'iconClassAdded' => 'text-danger'],
-                    ['label' => 'Warning', 'iconClass' => 'nav-icon far fa-circle text-warning'],
-                    ['label' => 'Informational', 'iconStyle' => 'far', 'iconClassAdded' => 'text-info'],
+                    [
+                        'label' => 'Clients',
+                        'icon' => 'fa-solid fa-user',
+                    ],
+                    [
+                        'label' => 'Balance Requests',
+                        'icon' => 'fa-solid fa-comment-dollar',
+                    ],
+                    [
+                        'label' => 'Luggage',
+                        'icon' => 'fa-solid fa-suitcase-rolling',
+                    ],
+                    [
+                        'label' => 'Airports',
+                        'icon' => 'fa-solid fa-building',
+                    ],
+                    [
+                        'label' => 'Flights',
+                        'icon' => 'fa-solid fa-plane-departure',
+                    ],
+                    [
+                        'label' => 'Airplanes',
+                        'icon' => 'fa-solid fa-plane',
+                    ],
+                    [
+                        'label' => 'Tariffs',
+                        'icon' => 'fa-solid fa-dollar-sign',
+                    ],
+                    [
+                        'label' => 'Refunds',
+                        'icon' => 'fa-solid fa-money-bill',
+                    ],
                 ],
             ]);
             ?>
