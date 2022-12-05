82e20fcc9ef311fca3c6b7e71285296ec7faace5
TomasMartinsMoura <2211866@my.ipleiria.pt>
2022-12-05 17:04:56 +0000

    Ajustes no balance request

--- frontend/views/client/index.php
+++ frontend/views/client/index.php
@@ -13,24 +13,42 @@ $this->title = 'My Profile';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="client-index">
+    <div class="gtco-section">
+        <div class="gtco-container">
+            <div class="row">
+                <div class="col-4 text-center align-self-center">
+                    <i class='fas fa-fa-solid fa-user-tie'></i>
 
-    <h1><?= Html::encode($this->title) ?></h1>
+                    <?= Html::a('Create Balance Req', ['create'], ['class' => 'btn btn-success']) ?>
+                </div>
+                <div class="col">
+                    <div class="row">
+                        <div class="col-6">
+                            <h2>Current balance requests</h2>
+                        </div>
+                        <div class=" col">
+                            <h2 class="text-right"><a href="history">View history</a></h2>
+                        </div>
+                    </div>
+                    <?= GridView::widget([
+                        'dataProvider' => $dataProvider,
+                        'columns' => [
+                            ['class' => 'yii\grid\SerialColumn'],
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+                            'user_id',
+                            'balance',
+                            'application',
+                            [
+                                'class' => ActionColumn::className(),
+                                'template' => '{update}',
 
-            'user_id',
-            'balance',
-            'application',
-            [
-                'class' => ActionColumn::className(),
-                'template' => '{update}',
+                            ],
+                        ],
+                    ]); ?>
 
-            ],
-        ],
-    ]); ?>
-
+                </div>
+            </div>
+        </div>
+    </div>
 
 </div>
